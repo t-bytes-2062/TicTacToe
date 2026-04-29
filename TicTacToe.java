507cda46e5290490e7bf5cec2f2b9435acf91b1f
t-bytes-2062 <tj2062@srmist.edu.in>
2026-04-29 09:55:37 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,21 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+
+        char[][] board = new char[3][3];
+
+        // Initialize board
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        // Print board
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
